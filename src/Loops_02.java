1622952e4d7d18538eadc7636175c9f277b75628
nihat_08 <nihat.oezgoec@student.htldornbirn.at>
2024-09-18 17:36:39 +0200

    Loops_02 finished

---
+++ src/Loops_02.java
@@ -0,0 +1,10 @@
+public class Loops_02 {
+    public static void main(String[] args) {
+        // Zähle die geraden Ziffern zwischen 1 und 1000 zusammen -
+        // Tip: Starte den Loop bei 2 und erhöhe den Zählindex jeweils um 2.
+        for (int i = 2; i <= 1000 ; i+=2) {
+            System.out.println(i);
+
+        }
+    }
+}
