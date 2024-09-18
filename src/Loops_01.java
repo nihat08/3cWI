6f31f230ee4ffd838e789298d439937e82ef7377
nihat_08 <nihat.oezgoec@student.htldornbirn.at>
2024-09-18 17:43:38 +0200

    Loops überarbeitet

--- src/Loops_01.java
+++ src/Loops_01.java
@@ -2,10 +2,12 @@ public class Loops_01 {
     public static void main(String[] args) {
 
         // Zähle in einem For-Loop die Zahlen von 1 bis 100 (inklusive) zusammen
-
+        int sum = 0;
         for (int i = 1; i <= 100 ; i++) {
-            System.out.println(i);
-
+        sum +=i;
+            System.out.println("i = " + i);
         }
+        System.out.println("Summe = " + sum);
+
     }
 }
--- src/Loops_02.java
+++ src/Loops_02.java
@@ -2,9 +2,11 @@ public class Loops_02 {
     public static void main(String[] args) {
         // Zähle die geraden Ziffern zwischen 1 und 1000 zusammen -
         // Tip: Starte den Loop bei 2 und erhöhe den Zählindex jeweils um 2.
+        int sum = 0;
         for (int i = 2; i <= 1000 ; i+=2) {
-            System.out.println(i);
-
+            System.out.println("i = " + i);
+            sum+=i;
         }
+        System.out.println("Summe = " + sum);
     }
 }
