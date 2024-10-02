cdb2e23fad598d70981dbb64f1b66cb1154a2110
nihat_08 <nihat.oezgoec@student.htldornbirn.at>
2024-10-02 09:05:03 +0200

    Bankomat case 1 finished

--- src/Bankomat.java
+++ src/Bankomat.java
@@ -12,8 +12,8 @@ public class Bankomat {
         Scanner scanner = new Scanner(System.in);
 
 
-        double balance = 0.0;
-        System.out.println(balance);
+        int Kontostand = 0;
+        System.out.println(Kontostand);
         boolean running = true;
 
         while (running) {
@@ -24,7 +24,19 @@ public class Bankomat {
             System.out.println("2. Abheben");
             System.out.println("3. Kontostand anzeigen");
             System.out.println("4. Beenden");
+        int auswahl = (scanner.nextInt());
 
+        switch(auswahl) {
+            case 1:
+                System.out.println("Wie viel möchten Sie einzahlen?");
+                int Einzahlen = scanner.nextInt();
+                if (Einzahlen>0){
+                    Kontostand+=Einzahlen;
+                    System.out.println(Einzahlen + "€ wurden eingezahlt");
+                }
+
+
+        }
         }
     }
 }
