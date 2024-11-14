e9c1721a7475d9d0093075f8371b3e37a626e969
nihat_08 <nihat.oezgoec@student.htldornbirn.at>
2024-11-14 08:15:26 +0100

    OO Getter and Setter finished

--- src/at/nihat/projects/OO/Car.java
+++ src/at/nihat/projects/OO/Car.java
@@ -3,41 +3,104 @@ package at.nihat.projects.OO;
 public class Car {
         //Instanz/Gedächnis
         //dont do that later
-        public int fuelMaxAmount;
-        public int fuelAmount;
-        public int fuelConsumption;
-        public String brand;
-        public String serialNumber;
-        public int speed;
-        public int BreakSpeed;
+        private int fuelMaxAmount;
+        private int fuelAmount;
+        private int fuelConsumption;
+        private String brand;
+        private String serialNumber;
+        private int speed;
+        private int BreakSpeed;
         private String color;
 
-        public Car (int fuelConsumption, String brand, String serialNumber){
-            this.fuelConsumption = fuelConsumption;
-            this.brand = brand;
-            this.serialNumber = serialNumber;
-        }
+    public Car (int fuelConsumption, String brand, String serialNumber){
+        this.fuelConsumption = fuelConsumption;
+        this.brand = brand;
+        this.serialNumber = serialNumber;
+        this.fuelMaxAmount = 100;
+        this.fuelConsumption = 7;
 
-        //Methode
-        public void drive(){
+    }
+
+    public Car(){
+
+    }
+
+
+
+    //Methode
+        public void getDrive(){
             this.fuelAmount = this.fuelAmount - this.fuelConsumption;
             System.out.println("I am driving");
             }
-        public void breakk(){
-          this.speed = this.speed - this.BreakSpeed;
+        public void getBreakk(){
            System.out.println("Ich bremse");
         }
-        public void turboBoost(){
+        public void getTurboBoost(){
             if (fuelAmount > fuelMaxAmount * 0.10){
                 System.out.println("SuperBoostMode");
             } else {
                 System.out.println("Not enough fuel to go to Superboost");
             }}
-        public void honk(int amountOfRepetitions){
-            System.out.println("Tuuut");
+        public void getHonk(int amountOfRepetitions){
+
+                System.out.println("Tuuut");
         }
         public void getRemainingRange(){
             int getRemainingRange = fuelAmount / fuelConsumption * 100;
             System.out.println("remaining range = " + getRemainingRange);
         }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
     }
+    public void setFuelAmount(int fuelAmount) {
+            if (fuelAmount>100) {
+                this.fuelAmount = 100;
+            } else {
+                this.fuelAmount = fuelAmount;
+            }
+        this.fuelAmount = fuelAmount;
+    }
+    public void setBreakSpeed(int breakSpeed) {
+        BreakSpeed = breakSpeed;
+    }
+    public void setColor(String color) {
+        this.color = color;
+    }
+    public void setFuelConsumption(int fuelConsumption) {
+        this.fuelConsumption = fuelConsumption;
+    }
+    public void setFuelMaxAmount(int fuelMaxAmount) {
+        this.fuelMaxAmount = fuelMaxAmount;
+    }
+    public void setSerialNumber(String serialNumber) {
+        this.serialNumber = serialNumber;
+    }
+    public void setSpeed(int speed) {
+        this.speed = speed;
+    }
+    public int getFuelAmount() {
+        return fuelAmount;
+    }
+    public int getFuelConsumption() {
+        return fuelConsumption;
+    }
+    public int getFuelMaxAmount() {
+        return fuelMaxAmount;
+    }
+    public String getBrand() {
+        return brand;
+    }
+    public int getBreakSpeed() {
+        return BreakSpeed;
+    }
+    public String getSerialNumber() {
+        return serialNumber;
+    }
+    public int getSpeed() {
+        return speed;
+    }
+    public String getColor() {
+        return color;
+    }
+}
--- src/at/nihat/projects/OO/Main.java
+++ src/at/nihat/projects/OO/Main.java
@@ -3,43 +3,44 @@ package at.nihat.projects.OO;
 public class Main {
     public static void main(String[] args) {
         int a = 7;
-    /*
+
         Car c1 = new Car();
-        c1.brand = "Audi";
-        c1.fuelConsumption = 7;
-        c1.serialNumber = "A1234";
-        c1.fuelAmount = 70;
-        c1.fuelMaxAmount = 100;
-        c1.speed = 50;
-        c1.BreakSpeed = 5;
+        c1.setBrand("Audi");
+        c1.setFuelConsumption(7);
+        c1.setSerialNumber("A1234");
+        c1.setFuelAmount(70);
+        c1.setFuelMaxAmount(100);
+        c1.setSpeed(50);
+        c1.setBreakSpeed(5);
 
         Car c2 = new Car();
-        c2.brand = "BMW";
-        c2.fuelConsumption = 5;
-        c2.serialNumber = "B1234";
-        c2.fuelAmount = 20;
-        c2.fuelMaxAmount = 100;
-        c2.speed = 45;
-        c2.BreakSpeed = 5;
-    */
-        Car c3 = new Car(17, "Opel", "A1234");
+        c2.setBrand("BMW");
+        c2.setFuelConsumption(7);
+        c2.setSerialNumber("B1234");
+        c2.setFuelAmount(20);
+        c2.setFuelMaxAmount(100);
+        c2.setSpeed(45);
+        c2.setBreakSpeed(5);
 
 
-        /*
-        System.out.println(c2.fuelAmount);
-        c2.drive();
-        System.out.println(c2.fuelAmount);
 
-        c1.turboBoost();
-        c2.turboBoost();
+
+        System.out.println(c2.getFuelAmount());
+        c2.getDrive();
+        System.out.println(c1.getFuelAmount());
+
+        c1.getTurboBoost();
+        c2.getTurboBoost();
 
         c1.getRemainingRange();
         c2.getRemainingRange();
 
-        c1.breakk();
-        System.out.println(c1.speed);
-        c2.breakk();
-        */
+        c1.getBreakk();
+        System.out.println(c1.getSpeed());
+        c2.getBreakk();
+
+
+
     }
     }
 
