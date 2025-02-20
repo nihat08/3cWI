23fccb8fe37890804b80d63f410c670ea8c18006
nihat_08 <nihat.oezgoec@student.htldornbirn.at>
2025-02-20 08:59:34 +0100

    OO Calculator finished

--- src/at/nihat/projects/Calculator/mainCalc.java
+++ src/at/nihat/projects/Calculator/mainCalc.java
@@ -1,5 +1,10 @@
 package at.nihat.projects.Calculator;
 
 public class mainCalc {
-    basicArithmetic calculator = new basicArithmetic();
+    public static void main(String[] args) {
+        superCalc s = new superCalc();
+        int res = superCalc.wrzl(10);
+        System.out.println(res);
+    }
+
 }
---
+++ src/at/nihat/projects/Calculator/superCalc.java
@@ -0,0 +1,7 @@
+package at.nihat.projects.Calculator;
+
+public class superCalc extends basicArithmetic{
+    public static int wrzl(int a){
+        return (int) Math.sqrt(a);
+    }
+}
